0cec1b3cc62f45eb8aa29755d8a7e808ce6fbf6d
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-12-30 21:17:38 +0700

    chore(emotion-cache-ts and _document.tsx): :lipstick: prevent FOUC for chakra ui issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,42 @@
 import { ColorModeScript } from "@chakra-ui/react";
+// eslint-disable-next-line import/order
+import createEmotionServer from "@emotion/server/create-instance";
+
 // eslint-disable-next-line @next/next/no-document-import-in-page
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import NextDocument, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
 import React from "react";
 
+import emotionCache from "functions/lib/emotion-cache";
 import theme from "theme";
 
+const { extractCritical } = createEmotionServer(emotionCache);
+
 export default class Document extends NextDocument {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await NextDocument.getInitialProps(ctx);
+    const styles = extractCritical(initialProps.html);
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <style
+          key="emotion-css"
+          dangerouslySetInnerHTML={{ __html: styles.css }}
+          data-emotion-css={styles.ids.join(" ")}
+        />,
+      ],
+    };
+  }
+
   render() {
     return (
       <Html>
